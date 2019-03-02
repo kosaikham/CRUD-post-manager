d48616488353aba6d6fad1f7ddd6f1abf6eeb850
saikham <saikham.email@gmail.com>
2019-03-02 22:38:55 +0630

    - Added moment-locales-webpack-plugin to remove unused local files

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const config = require('./db');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./db");
 
-const users = require('./routes/user');
-const posts = require('./routes/post');
+const users = require("./routes/user");
+const posts = require("./routes/post");
 
-mongoose.connect(config.DB, { useNewUrlParser: true})
-    .then(
-        () => console.log('Db connected'),
-        err => console.log('DB connect Fail')
-    )
-
+mongoose
+  .connect(config.PDB, { useNewUrlParser: true })
+  .then(
+    () => console.log("Db connected"),
+    err => console.log("DB connect Fail")
+  );
 
 const app = express();
 
-const path = require('path')
+const path = require("path");
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'frontend/build')))
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/api/users', users);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/posts", posts);
 
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
-  })
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+});
 
-app.get('/',(req,res) => res.send('hello'));
+app.get("/", (req, res) => res.send("hello"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
---
+++ secret.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'DB_PRODUCTION': 'mongodb://sai:sai123@ds051334.mlab.com:51334/mypostmanager',
+    'SECRET': 'adfkjlqweir813990bexqweoQEWRQEG'
+}
